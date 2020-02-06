a1e577deb700041656f7fcf523ab95574c6bc3a9
Shakir73 <engr.abdul.bari.ansari@gmail.com>
2020-02-06 15:45:08 +0500

    error

--- ahr/ahr/doctype/job_opening/job_opening.py
+++ ahr/ahr/doctype/job_opening/job_opening.py
@@ -8,7 +8,7 @@ import frappe
 
 from frappe.website.website_generator import WebsiteGenerator
 from frappe import _
-from staffing_plan.staffing_plan import get_designation_counts, get_active_staffing_plan_details
+from ahr.doctype.staffing_plan.staffing_plan import get_designation_counts, get_active_staffing_plan_details
 
 class JobOpening(WebsiteGenerator):
 	website = frappe._dict(
